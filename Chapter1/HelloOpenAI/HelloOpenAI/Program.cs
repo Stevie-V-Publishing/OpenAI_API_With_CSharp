ed5cc7c8cb20d4f757b718dbd0472b80bb4dd5e7
Stevie V <urbanblaster11@gmail.com>
2024-08-22 15:23:22 -0600

    chapter 1
    first chapter

---
+++ Chapter1/HelloOpenAI/HelloOpenAI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace HelloOpenAI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{			
+			var key = "Your key goes here";
+			var requestJson = "{\"model\": \"gpt-4o\", \"messages\": [{\"role\": \"user\", \"content\": \"Hello World!\" } ] }";
+			var apiUrl = "https://api.openai.com/v1/chat/completions";
+
+			using (var client  = new HttpClient()) 
+			{
+				client.DefaultRequestHeaders.Add("Authorization", $"Bearer {key}");
+
+				HttpContent content = new StringContent(requestJson, Encoding.UTF8, "application/json");
+				HttpResponseMessage response = client.PostAsync(apiUrl, content).Result;
+
+				Console.Write(response.Content.ReadAsStringAsync().Result);
+
+			}
+		}
+	}
+}
